cb6d4e727d1e9c691d9ece70e15e1883ec6e6b8a
BRIAN YACK <BRIAN YACK>
2017-08-20 10:41:16 -0500

    pseudo code jscript

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Create a variable and assign it to randomly generated target number 
+
+
+
+// Create a counter for the # of Wins
+
+// Create a counter for the # of Losses
+
+
+
+
+// Create a variable that stores a randomly generated number between 1-12 for crystal 1
+	//on click event that that links to the total score
+
+// Create a variable that stores a randomly generated number between 1-12 for crystal 2
+	//on click event that that links to the total score
+
+// Create a variable that stores a randomly generated number between 1-12 for crystal 3
+	//on click event that that links to the total score
+
+// Create a variable that stores a randomly generated number between 1-12 for crystal 4
+	//on click event that that links to the total score
+
+
+// Create a variable that stores click events from each crystal being targeted
+
+//Set win if the total score = targeted number
+
+//Set loss if the total score !== targeted number (goes over it)
+
+//Automatically restart the randomly generated number and clear the total score
